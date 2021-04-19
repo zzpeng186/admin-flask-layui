a1eacfb5ec4bc42d604046390139baa7b6583612
zzpeng186 <zzpeng186@163.com>
2021-04-20 01:52:44 +0800

    前端文件

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
+    # return 'Hello World!'
 
 
 if __name__ == '__main__':
